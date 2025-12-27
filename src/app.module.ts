7c34c9065efe07f98fc89e7752ede924b87f21c0
Sooraj Gupta <soorajgupta00@gmail.com>
2025-12-27 21:15:44 +0530

    update

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,10 +1,13 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import { EpisodesModule } from './episodes/episodes.module';
+import { ConfigController } from './config/config.controller';
+import { TopicsModule } from './topics/topics.module';
 
 @Module({
-  imports: [],
-  controllers: [AppController],
+  imports: [EpisodesModule, TopicsModule],
+  controllers: [AppController, ConfigController],
   providers: [AppService],
 })
 export class AppModule {}
